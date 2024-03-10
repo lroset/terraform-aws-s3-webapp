fb99f9f81c47433159d6884bf4f37be3b3916490
lroset <162152798+lroset@users.noreply.github.com>
2024-03-10 18:26:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,38 +35,6 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket" {
-  depends_on = [
-    aws_s3_bucket_public_access_block.bucket,
-    aws_s3_bucket_ownership_controls.bucket,
-  ]
-  bucket = aws_s3_bucket.bucket.id
-
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_s3_object" "webapp" {
   acl          = "public-read"
   key          = "index.html"
